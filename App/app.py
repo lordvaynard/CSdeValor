7a22d97cd2c61261c9d95588a577deb79c02cef0
lordvaynard <115435916+lordvaynard@users.noreply.github.com>
2023-01-08 21:16:25 -0300

    Update app.py

--- App/app.py
+++ App/app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import base64
+import datetime
+from datetime import 
 
 def filedownloadcsv(df, season, league):
     csv = df.to_csv(index=False)
@@ -50,3 +52,26 @@ st.subheader("Dataframe: "+selected_league)
 st.dataframe(df)
 
 st.markdown(filedownloadcsv(df, selected_season, selected_league), unsafe_allow_html=True)
+
+st.title("Jogos do Dia")
+
+dia = st.date_input(
+    "Data de Análise",
+    date.today())
+
+def load_data_jogos():
+    data_jogos = pd.read_csv("https://github.com/futpythontrader/YouTube/blob/main/Jogos_do_Dia_FlashScore/"+str(dia)+"_Jogos_do_Dia_FlashScore.csv?raw=true")
+    
+    return data_jogos
+
+df_jogos = load_data_jogos()
+
+st.dataframe(df_jogos)
+
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="Base_de_Dados.csv">Download CSV File</a>'
+    return href
+
+st.markdown(filedownload(df_jogos), unsafe_allow_html=True)
