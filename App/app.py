741e828ae1149f323f2f102d85904952a1030878
lordvaynard <115435916+lordvaynard@users.noreply.github.com>
2023-01-08 22:32:54 -0300

    Update app.py

--- App/app.py
+++ App/app.py
@@ -73,7 +73,7 @@ st.dataframe(df_jogos)
 def filedownload(df, dia):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="Base_de_Dados - '+str(dia)+'.csv">Download CSV File</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download="Jogos_do_dia-'+str(dia)+'.csv">Download CSV File</a>'
     return href
 
 st.markdown(filedownload(df_jogos, dia), unsafe_allow_html=True)
