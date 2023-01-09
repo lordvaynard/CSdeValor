d84a7091c3014ec4b359b1a2bc4b2d8532082b01
lordvaynard <115435916+lordvaynard@users.noreply.github.com>
2023-01-08 21:15:15 -0300

    Update Jogos_do_dia.py

--- App/pages/Jogos_do_dia.py
+++ App/pages/Jogos_do_dia.py
@@ -18,3 +18,11 @@ def load_data_jogos():
 df_jogos = load_data_jogos()
 
 st.dataframe(df_jogos)
+
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="Base_de_Dados.csv">Download CSV File</a>'
+    return href
+
+st.markdown(filedownload(df_jogos), unsafe_allow_html=True)
